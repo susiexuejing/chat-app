b8c44abec91a3e4b77bfda87e676239f2d7e1308
susiexuejing <susiexuejing@example.com>
2026-08-10 22:50:49 +0800

    test: EF-38 Phase 1 - fix react-native mocks for MessageList rendering

--- client/__mocks__/react-native.js
+++ client/__mocks__/react-native.js
@@ -1,4 +1,23 @@
 // Minimal react-native mock for testing
+const React = require('react');
+
+const createMockComponent = (name) => {
+  const Component = React.forwardRef((props, ref) => {
+    return React.createElement(name, { ...props, ref });
+  });
+  Component.displayName = name;
+  return Component;
+};
+
+const ScrollView = React.forwardRef((props, ref) => {
+  // Attach scrollToEnd to the ref if it's an object
+  React.useImperativeHandle(ref, () => ({
+    scrollToEnd: jest.fn(),
+  }));
+  return React.createElement('ScrollView', props);
+});
+ScrollView.displayName = 'ScrollView';
+
 module.exports = {
   Platform: { OS: 'web' },
   AppState: {
@@ -8,13 +27,22 @@ module.exports = {
   Keyboard: {
     addListener: jest.fn(() => ({ remove: jest.fn() })),
   },
-  View: 'View',
-  Text: 'Text',
-  TouchableOpacity: 'TouchableOpacity',
-  ScrollView: 'ScrollView',
-  TextInput: 'TextInput',
+  View: createMockComponent('View'),
+  Text: createMockComponent('Text'),
+  TouchableOpacity: createMockComponent('TouchableOpacity'),
+  ScrollView,
+  TextInput: createMockComponent('TextInput'),
+  Image: createMockComponent('Image'),
+  ActivityIndicator: createMockComponent('ActivityIndicator'),
   StyleSheet: {
     create: (styles) => styles,
+    flatten: (styles) => {
+      if (!styles) return {};
+      if (Array.isArray(styles)) {
+        return Object.assign({}, ...styles.map((s) => (typeof s === 'object' ? s : {})));
+      }
+      return typeof styles === 'object' ? styles : {};
+    },
   },
   Alert: {
     alert: jest.fn(),
